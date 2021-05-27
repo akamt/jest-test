08ae53328681263b6c6ce4e2c11933f0a30d116c
akamt <akamt68@gmail.com>
2021-05-27 17:52:26 +0900

    テストを適当に追加した。

---
+++ test/pages/scroll.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '../testUtils'
+import ScrollPage from '../../pages/scroll'
+
+describe('Scroll page', () => {
+  it('matches snapshot', () => {
+    const { asFragment } = render(<ScrollPage />, {})
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('clicking button triggers alert', () => {
+    const { getByText } = render(<ScrollPage />, {})
+
+    const sectionText = getByText('Section1').textContent
+    expect(sectionText).toBe('Section1')
+  })
+})

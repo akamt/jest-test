e5404348e68cbde08cbaba09f507e04cd545469b
akamt <akamt68@gmail.com>
2021-06-06 17:07:55 +0900

    filterのstate部分を切り出し忘れていたので追加した。

--- src/components/todoFilter.tsx
+++ src/components/todoFilter.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import { useRecoilState } from 'recoil'
-import { todoListFilterState } from '../atoms/todo'
+import { selector, useRecoilState } from 'recoil'
+import { todoListFilterState, todoListState } from '../atoms/todo'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type ContainerProps = {}
+
+export const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState)
+    const list = get(todoListState)
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete)
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete)
+      default:
+        return list
+    }
+  },
+})
 
 const TodoListFilterContainer: React.FC<ContainerProps> = () => {
   const [filter, setFilter] = useRecoilState(todoListFilterState)
--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,9 +1,8 @@
-import { useRecoilValue, selector } from 'recoil'
+import { useRecoilValue } from 'recoil'
 import TodoItem from '../components/todoItem'
 import TodoListStats from '../components/todoStats'
-import TodoListFilter from '../components/todoFilter'
+import TodoListFilter, { filteredTodoListState } from '../components/todoFilter'
 import TodoItemCreator from '../components/todoCreater'
-import { todoListState, todoListFilterState } from '../atoms/todo'
 
 const TodoList = () => {
   const todoList = useRecoilValue(filteredTodoListState)
@@ -20,22 +19,5 @@ const TodoList = () => {
     </div>
   )
 }
-
-const filteredTodoListState = selector({
-  key: 'filteredTodoListState',
-  get: ({ get }) => {
-    const filter = get(todoListFilterState)
-    const list = get(todoListState)
-
-    switch (filter) {
-      case 'Show Completed':
-        return list.filter((item) => item.isComplete)
-      case 'Show Uncompleted':
-        return list.filter((item) => !item.isComplete)
-      default:
-        return list
-    }
-  },
-})
 
 export default TodoList
